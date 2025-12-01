f14c61d559985cef0998bbcc89b04621a34812a2
Rohit <rohitpeswani35@gmail.com>
2025-12-02 05:00:00 +0530

    feat: add database schemas

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./database/db.js";
+
 
 dotenv.config({});
 
+// call database connection here
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// default middleware
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors());
+ 
+
+ 
  
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
