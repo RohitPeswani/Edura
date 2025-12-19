506b88c91d3c22a46f684a722f0e94f68816a5e2
Rohit <rohitpeswani35@gmail.com>
2025-12-19 08:00:00 +0530

    feat : developed search functionality and protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import HeroSection from "./pages/student/HeroSection";
+import MainLayout from "./layout/MainLayout";
+import Courses from "./pages/student/Courses";
+import MyLearning from "./pages/student/MyLearning";
+import Profile from "./pages/student/Profile";
+import SearchPage from "./pages/student/SearchPage";
+import CourseDetail from "./pages/student/CourseDetail";
+import CourseProgress from "./pages/student/CourseProgress";
+import { ThemeProvider } from "./context/ThemeContext";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import AuthenticatedUser from "./components/auth/AuthenticatedUser";
+
 
 import MainLayout from "./layout/MainLayout";
 
@@ -7,13 +20,76 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            <Courses />
+          </>
+        ),
+      },
+      {
+        path: "login",
+        element: (
+          <AuthenticatedUser>
+            <Login />
+          </AuthenticatedUser>
+        ),
+      },
+      {
+        path: "my-learning",
+        element: (
+          <ProtectedRoute>
+            <MyLearning />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course/search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-detail/:courseId",
+        element: (
+          <ProtectedRoute>
+            <CourseDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-progress/:courseId",
+        element: (
+          <ProtectedRoute>
+            <PurchaseCourseProtectedRoute>
+            <CourseProgress />
+            </PurchaseCourseProtectedRoute>
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
 ]);
 
 function App() {
   return (
     <main>
+      <ThemeProvider>
       <RouterProvider router={appRouter} />
+      </ThemeProvider>
     </main>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,11 @@ const Custom = ({ children }) => {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={appStore}>
+      <Custom>
         <App />
+        <Toaster />
+      </Custom>
+    </Provider>
   </StrictMode>
 );
